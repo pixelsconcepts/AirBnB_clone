246b82014e2e061d149ce1062be11a0b1fbfdd5d
Munachi Iwundu <realjones2012@gmail.com>
2023-11-13 00:04:10 +0000

    updated console

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Unittest for basemodel file: class and methods"""
 
-import pep8
 import unittest
 import os
 from models import base_model
@@ -10,15 +9,13 @@ from models.user import User
 from datetime import datetime
 
 
-class Test_Base_Model_outputs(unittest.TestCase):
+class TestBaseModel(unittest.TestCase):
     """Test_Base_outputs test for Base class"""
 
     def test_unique_id(self):
         """
         test_unique_id method that test if id is unique
         """
-        instance1 = BaseModel()
-        instance2 = BaseModel()
         self.assertNotEqual(instance1, instance2)
 
     def test_id_type(self):
@@ -72,35 +69,6 @@ class Test_Base_Model_outputs(unittest.TestCase):
         self.assertEqual(updated_expected_format, updated_actual_format)
         self.assertEqual(created_expected_format, created_actual_format)
         self.assertEqual(class_attr_value_expected, class_attr_value_get)
-
-
-class TestBaseModelpep8(unittest.TestCase):
-    """Validate pep8"""
-
-    def test_pep8(self):
-        """test for base file and test_base file pep8"""
-        style = pep8.StyleGuide(quiet=True)
-        base_mod = "models/base_model.py"
-        test_base_mod = "tests/test_models/test_base_model.py"
-        result = style.check_files([base_mod, test_base_mod])
-        self.assertEqual(result.total_errors, 0)
-
-
-class TestDocsBaseModel(unittest.TestCase):
-    """test docstrings for base and test_base files"""
-
-    def test_module(self):
-        """check module docstrings"""
-        self.assertTrue(len(base_model.__doc__) > 0)
-
-    def test_class(self):
-        """check class docstrings"""
-        self.assertTrue(len(BaseModel.__doc__) > 0)
-
-    def test_method(self):
-        """check method docstrings"""
-        for func in dir(BaseModel):
-            self.assertTrue(len(func.__doc__) > 0)
 
 
 if __name__ == "__main__":
