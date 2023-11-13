a858e29eb111ed3b7a62f6932f493a79d971b642
Munachi Iwundu <realjones2012@gmail.com>
2023-11-13 23:41:42 +0000

    added other classes

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Amenity class"""
+
+
+class Amenity(BaseModel):
+    """Amenity class inheriting BaseMode"""
+
+    def __init__(self):
+        """Initializes amenity class instance"""
+        super().__init__()
+    pass
